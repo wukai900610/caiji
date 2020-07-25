52bcf82d8ed66c14cac8554e06f94f103fd49b3a
z_笔记 <744982965@qq.com>
2020-07-26 07:35:26 +0800

    优化

--- new1688/main/index.js
+++ new1688/main/index.js
@@ -1,22 +1,37 @@
 var cheerio = require('cheerio');
 var fs = require('fs');
-var lib = require('../lib/index.js');
+var lib = require('../../lib/index.js');
 
 var ALL_CATEGORY_LIST_DATA;//所有分类商家列表
 var config = {
-    listNum:5205,//所有分类的位置
-    page:8,//当前分类的页数
+    listNum:5000,//所有分类的位置
+    page:1,//当前分类的页数
     tableName:'',
     cookie:'t=81a8c9537e3baecf7f42181aa3b9f6ab; cna=Ai0OF825fUACAd9EdAuQ40qG; ali_apache_id=11.179.217.73.1594900814588.463720.9; _ga=GA1.2.1276157287.1594997132; _gid=GA1.2.2072834857.1595592615; gangesweb-buckettest=112.0.89.187.1595592662508.7; xman_us_f=x_locale=en_US&x_l=1&last_popup_time=1595597944818&x_user=CA|Zhao|Lanser|ifm|243659941&no_popup_today=n; history=company%5E%0A709245414%24%0A243432804; acs_usuc_t=acs_rt=4296ec98077742d290b0a8bc719fa55f; cookie2=124f44a2257a7c7dc14ce680fbbad7df; _tb_token_=ff5e896ebe33f; _samesite_flag_=true; _bl_uid=Lgkkhd160jmundkOhe6ahwt4q1es; _hvn_login=4; csg=ed0b743b; xman_us_t=ctoken=3z9mm1mefs62&l_source=alibaba&x_user=CGLqrUMjw7i99AQzzD8hlKyOxa07J1/TrBw90eGldy8=&x_lid=ca1533434232dyhj&sign=y&need_popup=y; intl_locale=en_US; intl_common_forever=E+ti80ppTQg0DKGoh74Rzm+4d8vnJQKYwAMDMPufLC7ShO06LXnxQw==; xman_f=e111qJwK7lvmSgUbKV/VRqWtlKGFMLh3Fko5xDucuNoaCUVVKNRRJYUqfSNw/QXR3vOuACtYazfP0H8svcPEEdMdga5tsZjc4tUfezyWqrfKb4L1KVSq9aNPtx+QaS2LmIAuMs9UZ7Yju13lqFIQizfSRW+sSJAgAzlD6RKhOs8fb9ZH/nb/32N9mEidLoUduVBDSjhuscvNob/DZIiPmu7d1CxskEmhB+P292KCS8C5Epeq+iH0rlBU3YGHgJ0v63dzLO/PIs7f/vg33lrJSHpX6H44s7fyQxU4teTUS3Vt2GODVxvptbvJuemCPmiYq/QnswHB1C5s08jxdScwxMB308YF7caR8a28MVOMP0ShakaE+Pz3LMUE7yfVGK3KknNyS8M5zUYD6WDylvMj8A==; ali_apache_tracktmp=W_signed=Y; JSESSIONID=232BDF809031C8A3B9301F2B8263AE21; tfstk=c0K1BAmTA5VsdI34bdMF_fJnA1SVaYy5hV6vCe-cmQQxTWph9sm84_69bEVuvaBC.; ali_apache_track=mt=1|ms=|mid=ca1533434232dyhj; xman_t=bb58JSE1ePQbCI7h1GHlmeW2Qhq57tYJDwzFR2s/7qoK3bQrlIa6Lu3EY5xw3Mwxzd+JK7CQyr6TfKN31ox0gUBTtntO23/x4e/TH/uVcsiR9LWU84Wkel8QQKrRJweAeqLh5/uU8QEqh0xFGCHlxyQLvkM5Z3bKDFg+UUqNy5nHjYYjvl7vhOo02KAYT1i8/ufU3fMTje7U7hteF9cpeSXzQTXcZyaCcSCSWv8Oi0nkPMF0iLRPgs7614Saj2UkEeOlUfUCcLmMoNSR3tSUIkNkjGas5dsKOl5EouuOnm/vGa1rQisOc7zzw09hX+Bs6DuxIqWiwRj9axW+iFJ6V7qtEc6ttp8bsAY6hds8LFVJdcxsTzustJWejGzOGuHC6JoY3gKvzBL/PRFN2d5Xg0KnJ0FAVTxwp+mCMAHybmnpkhaQricc5fP03d/6PtpHhN3LvpN4KIXF5M9CLh9Pxn/eLqfJzperetA5FrZLoAex1TmvkYcl1yF3P5B2O4xhFFhBND4S3oy1Tlk9mGwkk2XSldW89vTx0TaZks4pIpwIfD7jwlHo1d2V+TnmWrpQhF/lUw6t79dQh37dpykHUtahzrLctVpaHjuSPK48xPQcpw2YtzcAeHkBoUVo8nT+s0ju+dUAj9au/G6Ge9MK/QO/faFJ1NO4mfiLNl+Eu1ifd7m2oubfSB+t6CHIF5Fiqhog1CGdv0w=; l=eBa7MUxlOLSlA3wwBO5Cnurza77TqIObzsPzaNbMiInca6_hZFOOpNQqIkRXodtjgtfvYetrb3kJjRFXP_UKgSUWolv4qG45cxJ6-; isg=BMDAtd3li5iDUXeXqwXtK2pKkU6SSaQTEeH73zpQwFtvtWLf4l1Ko_RDzR11BVzr',
 };
 
-// 开始
-fs.readFile('./data/links.json', 'utf8', function(err,data) {
+// 1.开始
+fs.readFile('../../1688/data/links.json', 'utf8', function(err,data) {
     ALL_CATEGORY_LIST_DATA = JSON.parse(data);//5262
-    lib.eventEmitter.emit('do_spider','init');
+
+    // 读取配置
+    fs.readFile('./config.json', 'utf8', function(err,data2) {
+        // console.log(data2);
+        if(err){
+            console.log('读取配置不存在，使用初始数据');
+            lib.eventEmitter.emit('do_spider','init');
+        }else{
+            // console.log(data2);
+            if(data2 != '' || data2 != undefined){
+                config = JSON.parse(data2);
+                // fs.unlinkSync('./config.json');
+                lib.eventEmitter.emit('do_spider','init');
+            }
+        }
+    });
 });
 
-// 创建数据表
+// 2.创建数据表
 var connection = lib.connection();
 connection.on('error', function(err) {
     console.log('db error', err);
@@ -27,7 +42,7 @@ connection.on('error', function(err) {
     }
 });
 
-// 获取新的分类商家列表url
+// 3.获取新的分类商家列表url
 lib.eventListener.on('do_spider', function(data) {
     config.tableName = '1688_supplys'+config.listNum;
     lib.creditSupplyTable(connection,config.tableName);
@@ -53,10 +68,18 @@ lib.eventListener.on('do_spider', function(data) {
 
 function endProgram() {
     console.log('中止程序,准备重启');
-    process.exit(0);//主动中止程序
+
+    fs.writeFile('./config.json', JSON.stringify(config), function(err) {
+        if (err) {
+            console.log(err);
+        }else{
+            console.log('中止程序前配置文件保存成功');
+            process.exit(0);//主动中止程序
+        }
+    });
 }
 
-// 列表
+// 4.列表
 async function getList() {
     var url = listUrl+config.page;
     console.log('1.列表url:'+url);
@@ -65,6 +88,11 @@ async function getList() {
     var $ = cheerio.load(result);
 
     var maxPage = parseInt($('.m-pagination .next').prev().text());
+    if(isNaN(maxPage)){
+        console.log('maxPage为空，页面被屏蔽');
+        endProgram();
+        return false;
+    }
     console.log('2.当前分类最大页数:'+maxPage+' 当前时间:'+lib.getTime());
     // console.log(maxPage);
     // return false;
@@ -77,13 +105,30 @@ async function getList() {
 
     var promiseArr = [];
     console.log('3.本页最大数量:'+$('.m-item').length);
+    console.log('4.当前分类:'+config.listNum);
     $('.m-item').each(function (index) {
         var supplyName = $(this).find('.title a').text().trim();
         var href = $(this).find('.title a').attr('href').replace('http','https');
         var connectUrl = $(this).find('.company .cd').attr('href').replace('http','https');
 
+        // 重写url
+        var start = href.indexOf('/member');
+        var end = href.indexOf('/company_profile');
+        if(start>0 && end>0){
+            var tempUrl = href.substring(start+8,end);
+            href = 'https://'+tempUrl+'.fm.alibaba.com/company_profile.html#top-nav-bar'
+        }
+
         // if(index == 0){
             var promiseFun = new Promise((resolve, reject) => {
+                // var params = {
+                //     index:index,
+                //     fullCategory:fullCategory.join('>'),
+                //     supplyName:supplyName,
+                //     href:href,
+                //     connectUrl:connectUrl,
+                // };
+                // getDetail(params,resolve,reject);
                 setTimeout(function () {
                     var params = {
                         index:index,
@@ -114,16 +159,17 @@ async function getList() {
             config.page=1;
             lib.eventEmitter.emit('do_spider');
         }else{
-            setTimeout(function () {
-                getList();
-            }, 2500);
+            // setTimeout(function () {
+            //     getList();
+            // }, 2500);
+            getList();
         }
     }).catch((e)=>{
         console.log(e);
     });
 }
 
-// 内容
+// 5.内容
 async function getDetail(obj,resolve,reject) {
     var result = await lib.myHttps(obj.href);
     var $ = cheerio.load(result);
@@ -131,6 +177,7 @@ async function getDetail(obj,resolve,reject) {
     // console.log(obj.href + ' ' + obj.index);
     // console.log('请求内容url'+obj.index+'成功');
     // console.log('');
+
     var supply = {};
     supply.supplyName = obj.supplyName;
 
